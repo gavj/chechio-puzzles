d7fc162cf83114771a0ab6a49dab5b3bef67d638
gavj <00gavin@gmail.com>
2015-10-09 00:39:31 -0600

    checkio - flat dictionary
    task 8

--- flat-dictionary.py
+++ flat-dictionary.py
@@ -1,1 +1,14 @@
 
+def flatten(dictionary):
+    stack = [((), dictionary)]
+    result = {}
+    while stack:
+        path, current = stack.pop()
+        for k, v in current.items():
+            if not v:
+                result["/".join((path + (k,)))] = ""
+            elif isinstance(v, dict):
+                stack.append((path + (k,), v))
+            else:
+                result["/".join((path + (k,)))] = v
+    return result
